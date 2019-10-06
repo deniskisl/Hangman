ac4145edee6e1902039104b72129cc09233fbea7
Kislitsin Denis <dkislitzin@gmail.com>
2019-10-07 00:20:01 +0700

    Add word reading function

---
+++ src/func.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "input.h"
+
+void read_dictionary(char *filename, char words[RSIZ][LSIZ], int *n)
+{
+    int count = 0;
+    FILE *fptr = NULL;
+    char word[LSIZ];
+    fptr = fopen(filename, "r");
+    if (fptr == NULL)
+    {
+        printf("Не получилось открыть файл\n");
+        exit(1);
+    }
+
+    fscanf(fptr, "%d\n", &count);
+    for (int i = 0; i < count; i++)
+    {
+        fscanf(fptr, "%s\n", words[i]);
+    }
+    *n = count;
+
+    fclose(fptr);
+}
+
+void hangman(int i)
+{
+    switch (i)
+    {
+    case 0:
+        printf("\n\t||===== ");
+        printf("\n\t||    | ");
+        printf("\n\t||      ");
+        printf("\n\t||      ");
+        printf("\n\t||      ");
+        printf("\n\t||      ");
+        break;
+    case 1:
+        printf("\n\t||===== ");
+        printf("\n\t||    | ");
+        printf("\n\t||    O ");
+        printf("\n\t||      ");
+        printf("\n\t||      ");
+        printf("\n\t||      ");
+        break;
+    case 2:
+        printf("\n\t||===== ");
+        printf("\n\t||    | ");
+        printf("\n\t||   %cO ", '\\');
+        printf("\n\t||      ");
+        printf("\n\t||      ");
+        printf("\n\t||      ");
+        break;
+    case 3:
+        printf("\n\t||===== ");
+        printf("\n\t||    | ");
+        printf("\n\t||   %cO/", '\\');
+        printf("\n\t||      ");
+        printf("\n\t||      ");
+        printf("\n\t||      ");
+        break;
+    case 4:
+        printf("\n\t||===== ");
+        printf("\n\t||    | ");
+        printf("\n\t||   %cO/", '\\');
+        printf("\n\t||    | ");
+        printf("\n\t||      ");
+        printf("\n\t||      ");
+        break;
+    case 5:
+        printf("\n\t||===== ");
+        printf("\n\t||    | ");
+        printf("\n\t||   %cO/", '\\');
+        printf("\n\t||    | ");
+        printf("\n\t||     %c", '\\');
+        printf("\n\t||      ");
+        break;
+    case 6:
+        printf("\n\t||===== ");
+        printf("\n\t||    | ");
+        printf("\n\t||   %cO/", '\\');
+        printf("\n\t||    | ");
+        printf("\n\t||   / %c", '\\');
+        printf("\n\t||      ");
+        break;
+    }
+}
---
+++ src/func.h
@@ -0,0 +1,4 @@
+#define LSIZ 20
+#define RSIZ 100
+
+void read_dictionary(char *filename, char words[RSIZ][LSIZ], int *n);
