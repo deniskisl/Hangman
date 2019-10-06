524f1e3749cccc8fbd4979b3147972d49269fb85
Kislitsin Denis <dkislitzin@gmail.com>
2019-10-07 01:38:44 +0700

    Select a random word from the list

--- src/main.c
+++ src/main.c
@@ -8,12 +8,12 @@ int main()
     int i = 0;
     int n = 0;
     char words[RSIZ][LSIZ];
+    char *word;
 
+    srand(time(0));
     read_dictionary("dictionary1.txt", words, &n);
-    for(i; i < n ;i++)
-    {
-    printf("%s\n", words[i]);
-    }
-    
+    int r = getrand(0, n);
+    word = words[r];
+    printf("%s\n", word);
     return 0;
 }
